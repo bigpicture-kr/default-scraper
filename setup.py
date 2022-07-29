22e4b18242d1aabfdceeef9ad233601f00c8dd07
Seongbum Seo <seo@seongbum.com>
2022-07-29 16:11:10 +0900

    Fix typo

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ def requirements():
         return f.read().splitlines()
 
 setuptools.setup(
-    name="Default Scrapper",
+    name="default-scraper",
     version="1.0.0",
     license="MIT",
     author="Seongbum Seo",
     author_email="sbumseo@bigpicture.team",
-    description="Web Scrapper",
+    description="Web Scraper",
     long_description=open("README.md").read(),
-    url="https://github.com/bigpicture-kr/default-scrapper",
+    url="https://github.com/bigpicture-kr/default-scraper",
     packages=setuptools.find_packages(),
     include_package_data=True,
     package_data={
-        "default_scrapper": ["drivers/chromedriver"],
+        "default_scraper": ["drivers/chromedriver"],
     },
     zip_safe=True,
     install_requires=requirements(),
